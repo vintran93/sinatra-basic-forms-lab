a7ba83c30430cdf1688490fc3661270335b481a1
vincent <vincentuong@yahoo.com>
2021-09-03 21:13:31 -0500

    commit tree c9fde7d8b81296adf936f39839852f06a46ea27b

--- app.rb
+++ app.rb
@@ -2,4 +2,21 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
 
+    # sends a 200 status code
+    # has a link with the text "List A Puppy"
+    # has a link to list a puppy that links to /new
+    get '/' do 
+        erb :index
+    end
+
+    # sends a 200 status code
+    # renders a form that can POST a name, breed, and age
+    get '/new' do
+        erb :create_puppy
+    end
+
+    post '/puppy' do
+        @puppy = Puppy.new(params['name'], params['breed'], params['age'])
+        erb :display_puppy
+    end
 end
